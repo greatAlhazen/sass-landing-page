3984493fb51fdaa943339764f71977d8763b59f8
Enes Uraz <axeprime91@gmail.com>
2023-01-01 10:16:14 +0300

    create smooth and creative navbar and review all code

--- app.js
+++ app.js
@@ -3,4 +3,28 @@ $(document).ready(function(){
         $('.navbarList').toggleClass('active')
     });
 
+    $(window).on('scroll load',function(){
+         $('.navbarList').removeClass('active');
+
+         if($(window).scrollTop() > 20 ){
+            $('.navbar').addClass('active');
+           console.log($('navbar'));
+         }else{
+            $('.navbar').removeClass('active');
+         };
+
+         $('section').each(function(){
+            const id = $(this).attr('id');
+            const height = $(this).height();
+            const offset = $(this).offset().top -200;
+            const top = $(window).scrollTop();
+            if(top >= offset && top < offset + height){
+                $('.navbarList li a').removeClass('active');
+                $('.navbarList').find('[data-scroll="' + id + '"').addClass('active');
+            }
+         }) 
+    });
+
+    
+
 });
